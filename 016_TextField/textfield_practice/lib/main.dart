52cf4d523962dfdbbba8c39125dd42d9629f83f7
raihanerli <raihanerlianti@gmail.com>
2022-10-13 16:50:06 +0700

    belajar textfield xixi

---
+++ 016_TextField/textfield_practice/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Latihan Text Field'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextField(
+                // obscureText: true, // untuk password
+                // maxLength: 5, // untuk jumlah karakter maksimal yang akan diinput
+                // maxLines: 2, // untuk jumlah baris maksimal yang disediakan ketika box telah sepenuhnya digunakan.
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: controller,
+              ),
+              Text(controller.text),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
