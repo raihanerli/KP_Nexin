aa552047b662d82527c6e94ddf696b6314142bc1
raihanerli <raihanerlianti@gmail.com>
2022-10-13 16:51:29 +0700

    tambahan

--- 016_TextField/textfield_practice/lib/main.dart
+++ 016_TextField/textfield_practice/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -17,10 +17,10 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Latihan Text Field'),
+          title: const Text('Latihan Text Field'),
         ),
         body: Container(
-          margin: EdgeInsets.all(20),
+          margin: const EdgeInsets.all(20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
