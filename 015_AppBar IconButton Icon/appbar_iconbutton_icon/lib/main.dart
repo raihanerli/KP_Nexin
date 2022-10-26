2c84c06380e2f891572ed4194c00e9e50a525830
raihanerli <raihanerlianti@gmail.com>
2022-10-26 13:40:31 +0700

    save latihan card

--- 015_AppBar IconButton Icon/appbar_iconbutton_icon/lib/main.dart
+++ 015_AppBar IconButton Icon/appbar_iconbutton_icon/lib/main.dart
@@ -1,184 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      home: MyWidget(),
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainPage(),
       debugShowCheckedModeBanner: false,
-    ));
-
-double collapsableHeight = 0.0;
-Color selected = const Color(0xffffffff);
-Color notSelected = const Color(0x0affffff);
-
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
+    );
+  }
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Custom Card Example",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: const Color(0xffD9D9D9),
+      ),
       body: Stack(
-        children: [
+        children: <Widget>[
           Container(
-            color: Colors.white,
-          ),
-          AnimatedContainer(
-            margin: const EdgeInsets.only(top: 79.0),
-            duration: const Duration(milliseconds: 375),
-            curve: Curves.ease,
-            height: (width < 800.0) ? collapsableHeight : 0.0,
-            width: double.infinity,
-            color: const Color(0xff121212),
-            child: SingleChildScrollView(
-              child: Column(
-                children: navBarItems,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff000000),
+                  Color(0xffffffff),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
               ),
             ),
           ),
-          Container(
-            color: const Color(0xff121212),
-            height: 80.0,
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'NavBar',
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    color: Color(0xffffffff),
-                  ),
-                ),
-                LayoutBuilder(
-                  builder: (context, constraints) {
-                    if (width < 800.0) {
-                      return NavBarButton(
-                        onPressed: () {
-                          if (collapsableHeight == 0.0) {
-                            setState(() {
-                              collapsableHeight = 240.0;
-                            });
-                          } else if (collapsableHeight == 24.0) {
-                            setState(() {
-                              collapsableHeight = 240.0;
-                            });
-                          }
-                        },
-                      );
-                    } else {
-                      return Row(
-                        children: navBarItems,
-                      );
-                    }
-                  },
-                )
-              ],
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: const Card(),
             ),
-          ),
+          )
         ],
-      ),
-    );
-  }
-}
-
-List<Widget> navBarItems = [
-  const NavBarItem(
-    text: 'About',
-  ),
-  const NavBarItem(
-    text: 'Explore',
-  ),
-  const NavBarItem(
-    text: 'Search',
-  ),
-  const NavBarItem(
-    text: 'Help',
-  ),
-];
-
-class NavBarItem extends StatefulWidget {
-  final String text;
-  const NavBarItem({super.key, 
-    required this.text,
-  });
-
-  @override
-  State<NavBarItem> createState() => _NavBarItemState();
-}
-
-class _NavBarItemState extends State<NavBarItem> {
-  Color color = notSelected;
-  @override
-  Widget build(BuildContext context) {
-    return MouseRegion(
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          splashColor: Colors.white60,
-          onTap: () {},
-          child: Container(
-            height: 60.0,
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Text(
-              widget.text,
-              style: const TextStyle(
-                fontSize: 16.0,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class NavBarButton extends StatefulWidget {
-  final Function onPressed;
-  const NavBarButton({super.key, 
-    required this.onPressed,
-  });
-
-  @override
-  State<NavBarButton> createState() => _NavBarButtonState();
-}
-
-class _NavBarButtonState extends State<NavBarButton> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 55.0,
-      width: 60.0,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: const Color(0xcfffffff),
-          width: 2.0,
-        ),
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      child: Material(
-        color: Colors.transparent,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        child: InkWell(
-          splashColor: Colors.white60,
-          onTap: () {
-            setState(() {
-              widget.onPressed();
-            });
-          },
-          child: const Icon(
-            Icons.menu,
-            size: 30.0,
-            color: Color(0xcfffffff),
-          ),
-        ),
       ),
     );
   }
